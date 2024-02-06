3ae7b4c7e986de27ae6eca8ee916f8e3fdf2fd1f
syarifh-id <125syarifh@gmail.com>
2024-02-06 08:04:16 +0700

    Cara dalam membuat Component

--- latihan-react1/src/App.tsx
+++ latihan-react1/src/App.tsx
@@ -1,7 +1,41 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button
+        className="rounded bg-black text-white font-semibold h-10 px-6"
+        type="button">
+        Click Me!
+      </button>
+    );
+  }
+}
+
+function Buttons() {
+  return (
+    <button className="rounded-md bg-red-600 text-black  px-6 h-10">
+      Click Me
+    </button>
+  );
+}
+
+const Title = () => {
+  return (
+    <span className="bg-slate-300 px-6 h-10 pt-2 font-semibold">
+      Saya Adalah Judul
+    </span>
+  );
+};
+
 const App = () => {
   return (
-    <div className="flex justify-end">
-      <h1>HEllow</h1>
+    <div className="flex justify-center items-center bg-blue-500 min-h-screen gap-3">
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Buttons></Buttons>
+      <Title></Title>
     </div>
   );
 };
